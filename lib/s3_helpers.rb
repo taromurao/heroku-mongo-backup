2ae6a70e0612d17ffa2d40030dc220411339ce4b
Taro Murao <taro.murao@gmail.com>
2012-10-05 22:21:41 +0900

    Update lib/s3_helpers.rb

--- lib/s3_helpers.rb
+++ lib/s3_helpers.rb
@@ -76,8 +76,8 @@ if defined?(AWS)
   end
 
   def HerokuMongoBackup::remove_old_backup_files(bucket_name, files_number_to_leave)
-    object_keys = Bucket.find(bucket_name).objects.map { |o| o.key }.sort
-    (0..excess-1).each { |i| S3Object.find(object_keys[i],bucket_name).delete } if (excess = object_keys.count - files_number_to_leave) > 0
+    object_keys = ( bucket = service.buckets.find(bucket_name) ).objects.map { |o| o.key }.sort
+    (0..excess-1).each { |i| bucket.objects.find(object_keys[i]).destroy } if (excess = object_keys.count - files_number_to_leave) > 0
   end
 
 end
